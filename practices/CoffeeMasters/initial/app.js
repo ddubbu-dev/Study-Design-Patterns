0fc7f21556514b402cc413c4eaf30bf9843c372b
ddubbu-dev <dev.ddubbu@gmail.com>
2025-05-12 10:33:17 +0900

    chore(실습): Prettier 적용 - CoffeeMasters

--- practices/CoffeeMasters/initial/app.js
+++ practices/CoffeeMasters/initial/app.js
@@ -21,8 +21,8 @@ app.router = Router;
 app.store = Store;
 
 window.addEventListener("DOMContentLoaded", () => {
-    app.router.init();
-    loadData();
+  app.router.init();
+  loadData();
 });
 
 
@@ -30,8 +30,8 @@ navigator.serviceWorker.register("/serviceworker.js");
 
 
 window.addEventListener("appcartchange", event => {
-    const badge = document.getElementById("badge");
-    const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
-    badge.textContent = qty;
-    badge.hidden = qty == 0;
+  const badge = document.getElementById("badge");
+  const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
+  badge.textContent = qty;
+  badge.hidden = qty == 0;
 });
